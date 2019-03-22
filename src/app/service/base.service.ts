89e05f07ffafcd9dc957f9ca67d24b3e67a22834
Kalle Johansson <kajoh@raa.se>
2019-03-22 12:43:25 +0100

    helloworld backkend

--- src/app/service/base.service.ts
+++ src/app/service/base.service.ts
@@ -6,7 +6,8 @@ import { Injectable } from '@angular/core';
 export class BaseService {
 
   // baseUrl: string = 'http://localhost:8080';
-  baseUrl: string = 'http://helloworld2:8080';
+  baseUrl: string = 'http://helloworld';
+  // baseUrl: string = 'http://helloworld-spring.apps.testraa.se';
   constructor() { }
 
 }
